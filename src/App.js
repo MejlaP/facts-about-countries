143ad6ae11dfaa0b1a945e5e0e4c743ccfb093b3
mejlap <miloshp8@gmail.com>
2022-05-14 10:25:58 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { countries } from './countries';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const headline = 'Fakta o státech';
+const subtitle = 'Klikněte na vlajku státu pro získání náhodné informace o něm'
+
+const background = <img className='background'
+  alt='europe'
+  src='https://cdn.pixabay.com/photo/2018/05/08/13/56/globe-3383088_960_720.jpg' />
+
+let flags = []
+
+// zahrnuje elem. h1, h2, p s jednotl. faktem, a pole s vlajkami
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{headline}</h1>
+        <h2>{subtitle}</h2>
+        {background}
+        {/*<p> elem. pro zobrazeni informace*/}
+        <p id='information'></p>
+        {/*do divu vlozi pole s vlajkami*/}
+        <div className='countries'>
+          {flags}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+// prochazeni countries objektem, kazda zeme(vlajka) dostane atribut a pushne se do flags
+// posledni atribut onClick - event listener ktery zavola fci viewInformation
+for (const country in countries) {
+  flags.push(
+    <img
+      key={country}
+      className='country'
+      alt={country}
+      src={countries[country].flag}
+      ariaLabel={country}
+      role='button'
+      onClick={viewInformation}
+    />
+  )
+}
+
+// funkce event listener
+function viewInformation(event) {
+  // ziskani nazvu z atributu alt img elementu
+  const choosenCountry = event.target.alt
+  // z objektu countries vybere zvol.zemi
+  const choosenCountryInfo = countries[choosenCountry]
+  // vygeneruje nahodny index z podpole data
+  const randomCountryIndex = Math.floor(Math.random() * choosenCountryInfo.data.length)
+  // pouzije nahodny index pro pristup k prvku v poli data
+  const countryData = choosenCountryInfo.data[randomCountryIndex]
+  //vezme <p> kam vlozi udaj s faktem
+  document.getElementById('information').innerHTML = countryData
+}
+
+/*renderuje countryInformation*/
+ReactDOM.render(<App />, document.getElementById('app'))
+export {App as default}
---
+++ src/countries.js
@@ -0,0 +1,14 @@
+export const countries = {
+    countryCzech: {
+        data: ['Úředním jazykem je čeština', 'Nejvyšší horou ČR je Sněžka (1603 m n. m.)', 'Území ČR se dělí na 14 vyšších územních samosprávných celků – krajů', 'Oficiální měnou ČR je koruna česká (Kč)'],
+        flag: 'https://cdn.pixabay.com/photo/2020/02/26/02/33/czechia-4880480_960_720.png'
+    },
+    countryAustria: {
+        data: ['Rakousko se skládá se z devíti spolkových zemí', 'Přibližně 60 % země Rakouska je hornaté povahy', 'Rakousko vstoupilo do Evropské unie v roce 1995', 'Největší rakouskou řekou je Dunaj'],
+        flag: 'https://cdn.pixabay.com/photo/2015/11/04/14/07/flag-1022605_960_720.png'
+    },
+    countrySlovakia: {
+        data: ['Slovensko je vnitrozemský stát ležící ve střední Evropě', 'V současné době se Slovensko dělí na 8 samosprávných krajů', 'Populace Slovenska udává k roku 2018 podle oficiálních dat 5 445 087 obyvatel', 'Nejvyšším bodem Slovenska je ve Vysokých Tatrách Gerlachovský štít s výškou 2 655 m.'],
+        flag: 'https://cdn.pixabay.com/photo/2013/07/13/14/17/slovakia-162421_960_720.png'
+    }
+};
